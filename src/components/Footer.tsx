c1922dbbc97161cf5e554505e9740d1093e16e74
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-04-01 18:40:49 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default () => (
     <div class="flex" mt-3 gap-2>
       <a
         class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-amber-300 bg-amber px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:border-amber-400 hover:bg-amber-300 hover:text-slate-2"
-        href="http://mxnf.store/123/123.jpg"
+        href="http://img.iswl.tk/123/123.jpg"
         target="_blank"
         rel="noopener noreferrer">
         <svg
@@ -24,7 +24,7 @@ export default () => (
       </a>
       <a
         class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-gray-300 bg-gray-3 px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:border-gray-500 hover:bg-gray-6 hover:text-slate-2"
-        href="http://mxnf.store/123/Untitled-1.html"
+        href="http://img.iswl.tk/123/Untitled-1.html"
         target="_blank"
         rel="noopener noreferrer">
         <Github />
