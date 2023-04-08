2d435d46f1a528812a116cfea3be8c85e98bb8a3
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-04-08 18:41:15 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -334,7 +334,7 @@ export default () => {
         <details mb-4>
           <summary>
             <div class="flex justify-between items-end text-slate">
-              <p>高级设置    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://gpt4.iswl.tk/" style="text-align: right" color="#000000">Pro版已发，速度更快</a></p>
+              <p>高级设置    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://gpt4.iswl.tk/" style="text-align: right" color="#000000">Pro版已发，点我速度更快</a></p>
               {/* <div class="flex ml-auto items-center" text-sm>
                 <span class="online-dot mr-2.5 mt-0.03"></span>
                 <span>{online} 在线</span>
