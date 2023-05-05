2a278bcde0b355f8c7df9946de746575aa209024
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-05-05 20:41:38 +0800

    Delete contants.ts

--- src/contants.ts
+++
@@ -1,4 +0,0 @@
-export const OFFICIAL_ACCOUNT_LINK =
-  "https://i.postimg.cc/kgxnp4Bz/qrcode-for-gh-2f9c561cf713-430.jpg";
-export const PRO_URL = "https://a.qacht.pro";
-export const DAO = "https://nav.navav.top";
